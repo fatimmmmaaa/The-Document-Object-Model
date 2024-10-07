ca7561d7b3bea1bc3ab82582a3097cac381defe7
Fatima Rettea <fatimarettea@Fatimas-MacBook-Pro.local>
2024-10-06 19:25:55 -0700

    first commit

---
+++ script.js
@@ -0,0 +1,11 @@
+// console.log(DOM)
+
+const app = document.getElementById('app');
+console.log
+
+//getElementbyID
+const title = document.getElementById('title');
+title.style.backgroundColor = "gray";
+title.style.textAlign = "center";
+
+// comsole.log(title);
